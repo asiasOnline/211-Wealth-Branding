6ca914db0a994393dabe2ca3093958e9e8323c1a
Asia Thompson <contact@asiasonline.com>
2026-07-13 19:55:34 -0700

    Remove template asset files and code, add shadcn and react icons, add setup for public assets, and modify middleware to proxy

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,13 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div>
-      
+      <Image 
+        src="/logos/211-Wealth-primary-color-logo-png.png"
+        width={400}
+        height={400}
+        alt="211 Wealth Logo"
+        loading="eager"
+      />
     </div>
   );
 }
